ec2b5cffaa73235a0ccf3771744604fc0aa5a58e
Jon <jonrad@gmail.com>
2017-05-06 17:08:19 -0400

    Expression magic

--- FluentParsing.Specs/ExcelConfigurationspecs.cs
+++ FluentParsing.Specs/ExcelConfigurationspecs.cs
@@ -16,45 +16,82 @@ namespace FluentParsing.Specs
             static Stream stream;
 
             Establish context = () =>
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                stream = assembly.GetManifestResourceStream("FluentParsing.Specs.Excel.Names.xlsx");
-
-                configuration = new FluentExcel()
-                    .ForSheet("dud")
-                    .ParseRows<Sample>()
-                        .WithField(nameof(Sample.Name))
-                        .WithField(nameof(Sample.Age))
-                    .Build();
-            };
+                stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FluentParsing.Specs.Excel.Names.xlsx");
 
             Cleanup cleanup = () =>
                 stream?.Dispose();
 
-            Because of = () =>
-                results = configuration.RunFor(stream);
-
-            It parsed_successfully = () =>
-                results.Successful.ShouldEqual(true);
-
-            It returned_2_count = () =>
-                results.Count.ShouldEqual(2);
-
-            It returned_proper_first_result = () =>
+            class when_using_string_field_names
             {
-                var item1 = results.Items.First();
+                Establish context = () =>
+                    configuration = new FluentExcel()
+                        .ForSheet("dud")
+                        .ParseRows<Sample>()
+                            .WithField(nameof(Sample.Name))
+                            .WithField(nameof(Sample.Age))
+                        .Build();
 
-                item1.Name.ShouldEqual("Jon");
-                item1.Age.ShouldEqual(20);
-            };
+                Because of = () =>
+                    results = configuration.RunFor(stream);
 
-            It returned_proper_second_result = () =>
+                It parsed_successfully = () =>
+                    results.Successful.ShouldEqual(true);
+
+                It returned_2_count = () =>
+                    results.Count.ShouldEqual(2);
+
+                It returned_proper_first_result = () =>
+                {
+                    var item1 = results.Items.First();
+
+                    item1.Name.ShouldEqual("Jon");
+                    item1.Age.ShouldEqual(20);
+                };
+
+                It returned_proper_second_result = () =>
+                {
+                    var item2 = results.Items.Skip(1).First();
+
+                    item2.Name.ShouldEqual("Mike");
+                    item2.Age.ShouldEqual(25);
+                };
+            }
+
+            class when_using_expression_field_names
             {
-                var item2 = results.Items.Skip(1).First();
+                Establish context = () =>
+                    configuration = new FluentExcel()
+                        .ForSheet("dud")
+                        .ParseRows<Sample>()
+                            .WithField(s => s.Name)
+                            .WithField(s => s.Age)
+                        .Build();
 
-                item2.Name.ShouldEqual("Mike");
-                item2.Age.ShouldEqual(25);
-            };
+                Because of = () =>
+                    results = configuration.RunFor(stream);
+
+                It parsed_successfully = () =>
+                    results.Successful.ShouldEqual(true);
+
+                It returned_2_count = () =>
+                    results.Count.ShouldEqual(2);
+
+                It returned_proper_first_result = () =>
+                {
+                    var item1 = results.Items.First();
+
+                    item1.Name.ShouldEqual("Jon");
+                    item1.Age.ShouldEqual(20);
+                };
+
+                It returned_proper_second_result = () =>
+                {
+                    var item2 = results.Items.Skip(1).First();
+
+                    item2.Name.ShouldEqual("Mike");
+                    item2.Age.ShouldEqual(25);
+                };
+            }
         }
 
         class Sample
--- FluentParsing/FluentRow.cs
+++ FluentParsing/FluentRow.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace FluentParsing
 {
@@ -24,7 +25,12 @@ namespace FluentParsing
 
         public IFluentRow<T> WithField<TField>(Expression<Func<T, TField>> field)
         {
-            throw new NotImplementedException();
+            // TODO all the error checking
+            var member = field.Body as MemberExpression;
+
+            var propertyName = (member.Member as PropertyInfo).Name;
+
+            return WithField(propertyName);
         }
 
         public IExcelConfiguration<T> Build()
